089d1fead3a82b4d68c85fc84899adf75898d885
Yoan Popov <Y.Popov@student.tudelft.nl>
2024-12-17 18:40:34 +0100

    Enh: add histograms for notes

--- airflow/dags/links-dag.py
+++ airflow/dags/links-dag.py
@@ -8,6 +8,9 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 from datetime import datetime, timedelta
+
+from matplotlib.ticker import MaxNLocator
+
 from utilities.ListLinkScraper import link_scrape
 from airflow.utils.dates import days_ago
 from airflow.models.variable import Variable
@@ -35,7 +38,7 @@ with DAG(
         schedule_interval='*/10 * * * *',  # every 10 minutes
         catchup=False,
         max_active_runs=1,
-        max_active_tasks=2
+        max_active_tasks=3
 ) as links_dag:
     @task.branch(task_id="branch")
     def branch_func():
@@ -281,7 +284,52 @@ with DAG(
             file_path_violin_plots = os.path.join(current_dir, folder_name, file_name)
             plt.savefig(file_path_violin_plots, bbox_inches='tight')
 
-            
+            #############
+            ## NOTES HISTOGRAM
+            #############
+
+            notes = df_filtered['notes'].apply(lambda x: ast.literal_eval(x))
+
+            dictionary_notes = dict()
+
+            for note_list in notes:
+                for current_note in note_list:
+                    if not dictionary_notes.get(current_note):
+                        dictionary_notes[current_note] = 1
+                    else:
+                        dictionary_notes[current_note] += 1
+
+            num_unique_notes = len(dictionary_notes)
+            n = 25 if num_unique_notes > 25 else num_unique_notes
+
+            # select top n most frequently used notes (since it would not look nice if we displayed all notes)
+            top_notes = sorted(dictionary_notes.items(), key=lambda x: x[1], reverse=True)[:n]
+
+            notes = [item[0] for item in top_notes]
+            values = [item[1] for item in top_notes]
+
+            plt.figure(figsize=(16, 8))
+            plt.bar(notes, values, color="#C96868")
+            plt.xlabel('Notes')
+            plt.ylabel('Number of perfumes')
+            plt.title(f'Top {n} most frequently used notes')
+
+            # force y-axis to display only rounded numbers
+            plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+
+            plt.tight_layout()
+
+            plt.subplots_adjust(bottom=0.25)  # Increase the bottom margin
+
+            # add text below the x-axis label
+            plt.figtext(0.5, 0.05, f'Total number of unique notes in dataset: {num_unique_notes}', ha='center', fontsize=14)
+
+            # rotate ticks to reduce text overlap
+            plt.xticks(rotation=45, ha="right", fontsize=8)
+
+            file_name = f'{decade}_{gender}_notes_histogram.png'
+            file_path_notes_histogram = os.path.join(current_dir, folder_name, file_name)
+            plt.savefig(file_path_notes_histogram, bbox_inches='tight')
 
             #############
             ## PIE CHARTS
