e3c7d9ee747adc72ffdafc75307d1a1775d5f965
Yoan Popov <Y.Popov@student.tudelft.nl>
2024-12-18 01:05:19 +0100

    Enh: add almost all correlations

--- airflow/dags/links-dag.py
+++ airflow/dags/links-dag.py
@@ -181,6 +181,8 @@ with DAG(
                 print("DataFrame is empty!")
                 return
 
+            df_filtered = df_filtered.reset_index(drop=True)
+
             df_filtered_rubrics_only = df_filtered[['scent', 'longevity', 'sillage', 'bottle', 'value_for_money']]
 
             # Calculate statistics
@@ -359,6 +361,102 @@ with DAG(
             file_name = f'{decade}_{gender}_avg_categories_piecharts.png'
             file_path_categories_piecharts = os.path.join(current_dir, folder_name, file_name)
             plt.savefig(file_path_categories_piecharts, bbox_inches='tight')
+
+            ##############
+            ## CORRELATION (notes - rubrics)
+            ##############
+
+            # Step 1: Flatten the notes column into binary columns
+            df_filtered['notes'] = df_filtered['notes'].apply(lambda x: ast.literal_eval(x))
+            notes_dummies = df_filtered['notes'].explode().str.get_dummies().groupby(level=0).max()
+
+            # Step 1.1: Get the top n most frequent notes
+            n = 50
+            note_counts = df_filtered['notes'].explode().value_counts()
+            top_notes = note_counts.head(n).index
+
+            # Filter the notes dummies to include only the top n notes
+            notes_dummies_top = notes_dummies[top_notes]
+
+            # Step 2: Combine the binary top notes columns with the rubrics
+            df_combined = pd.concat([notes_dummies_top, df_filtered[['scent', 'longevity', 'sillage',  'value_for_money']]],
+                                    axis=1)
+
+            # Step 3: Compute the correlation matrix
+            correlation_matrix = df_combined.corr()
+
+            # Display correlations between top notes and rubrics
+            note_columns = notes_dummies_top.columns
+            rubric_columns = ['scent', 'longevity', 'sillage', 'value_for_money']
+
+            correlation_notes_rubrics = correlation_matrix.loc[note_columns, rubric_columns]
+
+            # Step 4: Visualize the correlation matrix using seaborn
+            plt.figure(figsize=(18, 16))
+            sns.heatmap(correlation_notes_rubrics, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+            plt.title(f"Correlation Between Top {n} Notes and Rubrics")
+
+            plt.tight_layout()
+            file_name = f'{decade}_{gender}_notes_rubrics_correlation.png'
+            file_path_notes_rubrics_correlation = os.path.join(current_dir, folder_name, file_name)
+            plt.savefig(file_path_notes_rubrics_correlation, bbox_inches='tight')
+
+            ##############
+            ## CORRELATION (notes - categories)
+            ##############
+
+            df_encoded_categories = pd.DataFrame()
+
+            for category in categories:
+                df_type = df_filtered[category].apply(lambda x: ast.literal_eval(x))
+                df_type_nums = df_filtered[f'{category}_numbers'].apply(lambda x: ast.literal_eval(x))
+
+                dictionary = dict()
+
+                for i, type_list in enumerate(df_type):
+                    for j, specific_type in enumerate(type_list):
+                        prefill_list = None
+                        dict_val = dictionary.get(specific_type)
+
+                        if not dict_val:
+                            prefill_list = [0] * len(df_type)
+                        else:
+                            prefill_list = dict_val
+
+                        prefill_list[i] = df_type_nums.iloc[i][j]
+                        dictionary[specific_type] = prefill_list
+
+                df_encoded_categories = pd.concat(
+                    [df_encoded_categories, pd.DataFrame(dictionary)], axis=1
+                )
+
+            # Step 2: Combine the binary top notes columns with the rubrics
+            df_combined = pd.concat(
+                [notes_dummies_top, df_encoded_categories],
+                axis=1)
+
+            print(df_combined)
+
+            # Step 3: Compute the correlation matrix
+            correlation_matrix = df_combined.corr()
+
+            print(correlation_matrix)
+
+            # Display correlations between top notes and rubrics
+            note_columns = notes_dummies_top.columns
+            category_columns = df_encoded_categories.columns
+
+            correlation_notes_categories = correlation_matrix.loc[note_columns, category_columns]
+
+            # Step 4: Visualize the correlation matrix using seaborn
+            plt.figure(figsize=(18, 16))
+            sns.heatmap(correlation_notes_categories, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+            plt.title(f"Correlation Between Top {n} Notes and Categories")
+
+            plt.tight_layout()
+            file_name = f'{decade}_{gender}_notes_categories_correlation.png'
+            file_path_notes_categories_correlation = os.path.join(current_dir, folder_name, file_name)
+            plt.savefig(file_path_notes_categories_correlation, bbox_inches='tight')
 
             ##############
             ## PROGRESSION
