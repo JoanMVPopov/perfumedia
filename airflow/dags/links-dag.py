112bcfbc75e18168bf83118d22c731e587565bea
Yoan Popov <Y.Popov@student.tudelft.nl>
2024-12-18 16:09:43 +0100

    Enh: add basic brand info

--- airflow/dags/links-dag.py
+++ airflow/dags/links-dag.py
@@ -483,10 +483,65 @@ with DAG(
             plt.savefig(file_path_notes_categories_correlation, bbox_inches='tight')
 
             ##############
+            ## BRANDS
+            ##############
+
+            # Aggregation step
+            df_brands_agg = df_filtered.groupby('brand')[
+                ['scent', 'longevity', 'sillage', 'bottle', 'value_for_money']].agg(['mean'])
+            df_counts = df_filtered.groupby('brand').size().reset_index(name='count')
+
+            # Flatten the MultiIndex for column labels in df_brands_agg
+            df_brands_agg.columns = [col[0] if isinstance(col, tuple) else col for col in df_brands_agg.columns]
+
+            # Merge counts with aggregated data
+            df_brands_agg = df_counts.merge(df_brands_agg, on='brand')
+
+            # Select the top n based on count
+            n = len(df_brands_agg) if (len(df_brands_agg) <= 10) else 10
+            df_brands_agg = df_brands_agg.sort_values('count', ascending=False).head(n)
+
+            df_brands_agg.reset_index(inplace=True, drop=True)
+
+            # Add note for superscript meaning
+            note = "Colored cells indicate highest value in column"
+
+            # Create the table
+            plt.figure(figsize=(10, 2))
+            current_table = plt.table(
+                cellText=df_brands_agg.iloc[:, 1:].values.round(2),
+                rowLabels=df_brands_agg.iloc[:, 0].values,
+                colLabels=df_brands_agg.columns[1:],
+                loc='center'
+            )
+
+            for i, row in df_brands_agg.iterrows():
+                for j, col in enumerate(df_brands_agg.columns[1:]):
+                    is_max = row[col] == df_brands_agg[col].max() if col != 'count' else False
+                    if is_max:
+                        cell = current_table.get_celld()[i + 1, j]  # +1 for header row
+                        cell.set_facecolor('#C96868')
+
+            # Add a note below the table
+            plt.text(0.5, 0.01, note, ha='center', fontsize=10)
+
+            # Adjust font size
+            current_table.auto_set_font_size(False)
+            current_table.set_fontsize(10)
+
+            # Hide the axes
+            plt.axis('off')
+
+            plt.tight_layout()
+            file_name = f'{decade}_{gender}_brands_stats.png'
+            file_path_brands_stats = os.path.join(current_dir, folder_name, file_name)
+            plt.savefig(file_path_brands_stats, bbox_inches='tight')
+
+            ##############
             ## PROGRESSION
             ##############
 
-            # only do it for all decades, per gender since info is not enough for further granularity
+            # only do it for all decades, per gender, since info is not enough for further granularity
             if decade == 'All':
                 # reset index otherwise rel_year becomes index
                 # selects all 5 rubric columns + years and excludes everything else
